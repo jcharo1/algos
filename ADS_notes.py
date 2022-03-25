4e05e1e6871ba466739e8d4e76f32b1f83a65cfc
Justin Charo <65956091+jcharo1@users.noreply.github.com>
2022-03-24 23:58:44 -0500

    Update ADS_notes.py

--- ADS_notes.py
+++ ADS_notes.py
@@ -1,7 +1,7 @@
 # Examples of iteration with for loops.
 
 # my_list = [0, 1, 2, 3, 4, 5] d d
-
+#
 # # Print each value in my_list. Note you can use the "in" keyword to iterate over a list.
 # for item in my_list:
 #     print('The value of item is: ' + str(item)) 
