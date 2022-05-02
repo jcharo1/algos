1333bbc91180a9ec9a7158fab8fec527edec66ba
Justin <justinrcharo@gmail.com>
2022-05-02 16:35:42 -0500

    file recurision

--- notes/Data_structures/trees/DFS_pre_order_traversal.py
+++ notes/Data_structures/trees/DFS_pre_order_traversal.py
@@ -123,26 +123,49 @@ class Stack():
 
 
 
-visit_order = []
-stack = Stack()
-node = tree.get_root()
-stack.push(node)
-visit_order.append(node.get_value())
-print(f"""visit order {visit_order}
-{stack}
-""")
-
-# check if apple (root) has a left child
-print(f"{node} has left child? {node.has_left_child()}")
-
-# since apple has a left child (banana)
-# we'll visit banana and add it to the stack
-if node.has_left_child():
-    node = node.get_left_child()
-    stack.push(node)
-
-print(f"""
-visit_order {visit_order} 
+def pre_order_with_stack(tree, debug_mode=False):
+    visit_order = list()
+    stack = Stack()
+    node = tree.get_root()
+    visit_order.append(node.get_value())
+    state = State(node)
+    stack.push(state)
+    count = 0
+    while(node):
+        if debug_mode:
+            print(f"""
+loop count: {count}
+current node: {node}
 stack:
 {stack}
-""")
+            """)
+        count +=1
+        if node.has_left_child() and not state.get_visited_left():
+            state.set_visited_left()
+            node = node.get_left_child()
+            visit_order.append(node.get_value())
+            state = State(node)
+            stack.push(state)
+
+        elif node.has_right_child() and not state.get_visited_right():
+            state.set_visited_right()
+            node = node.get_right_child()
+            visit_order.append(node.get_value())
+            state = State(node)
+
+        else:
+            stack.pop()
+            if not stack.is_empty():
+                state = stack.top()
+                node = state.get_node()
+            else:
+                node = None
+            
+    if debug_mode:
+            print(f"""
+loop count: {count}
+current node: {node}
+stack:
+{stack}
+            """)
+    return visit_order
