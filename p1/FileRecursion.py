4042f55cb32669dd1d8471670d22249ecc85c5de
Justin <justinrcharo@gmail.com>
2022-05-03 14:48:18 -0500

    algo project

--- p1/FileRecursion.py
+++ p1/FileRecursion.py
@@ -1,4 +1,7 @@
+import os
+
 def find_files(suffix, path):
+  
     """
     Find all files beneath path with file name suffix.
 
@@ -14,8 +17,36 @@ def find_files(suffix, path):
     Returns:
        a list of paths
     """
+    list_of_current_files = os.listdir(f"{path}")
+    files_found = []
+    for file in list_of_current_files:
+      
+      if os.path.isdir(f'./{file}'):   # is this a directory if so continue serarching
+        print(file)
+        find_files(suffix,path)
+      else:                               # else it is not a directory it is a file 
+
+
+       if os.path.isfile(f"./{file}"): 
+         print("i made it here")
+         if (f"{file}".endswith(suffix)): #check if it ends with suffix
+           print("and here tooo")
+           files_found.append(f"{file}") 
+      print(files_found)
+        
+
+    
+          
+
+    
+    
+    
+
     return None
 
+
+
+find_files(".c", "./testdir")
 # Add your own test cases: include at least three test cases
 # and two of them must include edge cases, such as null, empty or very large values
 
